b6a259540e31fef8876717df98fc49332c44f3a7
David Hauser <hauser-david@hotmail.com>
2025-02-12 22:53:11 +0100

    add echo statemets to slurm script

--- run0.sh
+++ run0.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --partition=boost_usr_prod
 #SBATCH --qos=boost_qos_lprod
-#SBATCH --time=01:00:00
+#SBATCH --time=48:00:00
 #SBATCH --chdir=/leonardo_scratch/fast/EUHPC_D18_005/david/FastV/src/LLaVA
 #SBATCH --output=/leonardo_scratch/fast/EUHPC_D18_005/david/outputs
 #SBATCH --error=/leonardo_scratch/fast/EUHPC_D18_005/david/outputs/fastv.err
@@ -15,13 +15,19 @@
 # watch -n 1 squeue --me
 
 # Interactive mode
-# salloc --nodes=1 --gres=gpu:1 --ntasks-per-node=1 --account=EUHPC_D18_005 --partition=boost_usr_prod
+# salloc --nodes=1 --gres=gpu:1 --ntasks-per-node=1 --cpus-per-task=8 --account=EUHPC_D18_005 --partition=boost_usr_prod
+
+echo "Setting HF_HOME variable..."
 
 # set HF home directory for offline datasets
 export HF_HOME="/leonardo_scratch/fast/EUHPC_D18_005/david/hf-datasets-cache"
 
+echo "Activating conda environment..."
+
 # activate conda env
 conda deactivate
 conda activate base
+
+echo "Running FastV..."
 
 srun /leonardo_scratch/fast/EUHPC_D18_005/david/FastV/src/LLaVA/lmms-evals.sh
