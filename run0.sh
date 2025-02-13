3076592aa5dc59434d13783837609d4aa9c8e5f8
David Hauser <hauser-david@hotmail.com>
2025-02-13 14:46:57 +0100

    fix run script

--- run0.sh
+++ run0.sh
@@ -17,18 +17,24 @@
 # Interactive mode
 # salloc --nodes=1 --gres=gpu:1 --ntasks-per-node=1 --cpus-per-task=8 --account=EUHPC_D18_005 --partition=boost_usr_prod
 
-echo "Setting HF_HOME variable..."
 
 # set HF home directory for offline datasets
 export HF_HOME="/leonardo_scratch/fast/EUHPC_D18_005/david/hf-datasets-cache"
 
-echo "Activating conda environment..."
-
-# activate conda env
+# Initialize conda
 conda init
+
+# Reload shell for conda to be available
+source ~/.bashrc
+
+# Install accelerate
+pip install accelerate
+
+# This is necessary for conda env to be recognized
 conda deactivate
 conda activate base
 
-echo "Running FastV..."
+# change directory to make relative model path work
+cd /leonardo_scratch/fast/EUHPC_D18_005/david/FastV/src/LLaVA
 
 srun /leonardo_scratch/fast/EUHPC_D18_005/david/FastV/src/LLaVA/lmms-evals.sh
