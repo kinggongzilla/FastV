f44a28edb9310b5ac65debf7a7ed653f5385bcdf
David Hauser <hauser-david@hotmail.com>
2025-03-09 09:18:21 +0100

    hf offline mode

--- run0.sh
+++ run0.sh
@@ -39,17 +39,9 @@ export HF_HOME="/leonardo_work/EUHPC_D18_005/david/hf-datasets-cache"
 # Change directory to make relative model path work
 cd /leonardo_scratch/fast/EUHPC_D18_005/david/FastV/src/LLaVA
 
-# Set the machine rank
-export RANK=$SLURM_PROCID
-
-# Set the master address and port
-export MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
-export MASTER_PORT=29500
-
-# Print the environment variables
-echo "MASTER_ADDR: $MASTER_ADDR"
-echo "MASTER_PORT: $MASTER_PORT"
-echo "RANK: $RANK"
+# HF offline mode
+export HF_DATASETS_OFFLINE=1
+export TRANSFORMERS_OFFLINE=1
 
 # Run the script
 /leonardo_scratch/fast/EUHPC_D18_005/david/FastV/src/LLaVA/lmms-evals.sh
